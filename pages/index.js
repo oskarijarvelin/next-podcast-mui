67d0254ec313d9d8802081d43619b4fc8f35d13a
Oskari Järvelin <contact@oskarijarvelin.fi>
2022-08-16 00:31:17 +0300

    Moved data to env

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,84 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import ProTip from '../src/ProTip';
-import Link from '../src/Link';
-import Copyright from '../src/Copyright';
+import * as React from "react";
+import Head from "next/head"
+import { Container, Typography, Box, Grid, Button } from "@mui/material";
+import AppBar from "../components/AppBar";
+import Copyright from "../src/Copyright";
+import parse from "html-react-parser";
+import Parser from "rss-parser";
+import Moment from "react-moment";
 
-export default function Index() {
+export default function Index({ feed, name }) {
+  const [showNumber, setShowNumber] = React.useState(6);
+
+  function increaseShowNumber() {
+    setShowNumber(showNumber + 6);
+  }
+
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Next.js example
-        </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
+    <>
+      <Head>
+        <title>{name}</title>
+      </Head>
+      <AppBar name={name} />
+      <Container maxWidth="xl">
+        <Box sx={{ my: 4 }}>
+          <Typography variant="h4" component="h1" gutterBottom>
+            {name}
+          </Typography>
+          <Grid container spacing={2}>
+            {feed.items.filter((item, index) => ((index < showNumber) ? true : false)).map((item) => (
+              <Grid item xs={12} sm={6} lg={4} key={item.link}>
+                <img
+                  src={item.image[0]["$"].href}
+                  alt={item.title}
+                  loading="lazy"
+                  width="100%"
+                  height="auto"
+                />
+                <h2>{item.title}</h2>
+                <p><Moment date={item.isoDate} format="DD.MM.YYYY" /> &bull; Tuotantokausi {item.season} &bull; Jakso {item.episode}</p>
+              </Grid>
+            ))}
+          </Grid>
+
+          {(showNumber < feed.items.length) ?
+            <Button variant="contained" onClick={increaseShowNumber}>Lataa lisää</Button>  : ''
+          }
+          
+          <Copyright />
+        </Box>
+      </Container>
+    </>
   );
+}
+
+async function getFeed(feedUrl) {
+  let parser = new Parser({
+    customFields: {
+      item: [
+        ["description", "content"],
+        ["itunes:explicit", "explicit"],
+        ["itunes:duration", "duration"],
+        ["itunes:image", "image", { keepArray: true }],
+        ["itunes:season", "season"],
+        ["itunes:episode", "episode"],
+        ["itunes:episodeType", "episodeType"],
+      ],
+    },
+  });
+  let feed = await parser.parseURL(feedUrl);
+  return feed;
+}
+
+export async function getStaticProps() {
+  const feedName = process.env.PODCAST_NAME
+  const detailedFeed = await getFeed(process.env.PODCAST_RSS_URL);
+
+  return {
+    props: {
+      feed: detailedFeed,
+      name: feedName
+    },
+    revalidate: 60,
+  };
 }
--- src/Copyright.js
+++ src/Copyright.js
@@ -4,12 +4,12 @@ import MuiLink from '@mui/material/Link';
 
 export default function Copyright() {
   return (
-    <Typography variant="body2" color="text.secondary" align="center">
+    <Typography variant="body2" color="text.secondary" align="center" marginTop={8}>
       {'Copyright © '}
+      {new Date().getFullYear()}{': '}
       <MuiLink color="inherit" href="https://mui.com/">
         Your Website
-      </MuiLink>{' '}
-      {new Date().getFullYear()}.
+      </MuiLink>
     </Typography>
   );
 }
